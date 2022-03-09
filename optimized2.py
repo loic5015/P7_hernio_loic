cdc1bf4d6a0499d5145de7ee492d2a8336b821f1
loic5015 <loic.hernio@gmail.com>
2022-03-09 13:38:16 +0100

    integration de l'algorythme sac a dos dynamique

--- optimized2.py
+++ optimized2.py
@@ -13,13 +13,9 @@ class Action:
         for key in dict_data:
             setattr(self, key, dict_data[key])
         self.benefit = self.calculate_gain()
-        self.unit_yield = self.calculate_yield()
 
     def calculate_gain(self):
         return self.cost*self.rate/100
-
-    def calculate_yield(self):
-        return self.benefit/self.cost
 
 
 class Controller:
@@ -35,45 +31,51 @@ class Controller:
         for x in df.index:
             if df.loc[x, "price"] <= 0:
                 df.drop(x, inplace=True)
+            else:
+                df.loc[x, "price"] = df.loc[x, "price"] * 100
+                df.loc[x, "profit"] = df.loc[x, "profit"]
         new_df = df.sort_values(by=['price'], ascending=False)
         for x in new_df.index:
-            action = Action({"name": new_df.loc[x, "name"], "cost": new_df.loc[x, "price"], "rate": new_df.loc[x, "profit"]})
+            action = Action({"name": new_df.loc[x, "name"], "cost": int(new_df.loc[x, "price"]),
+                             "rate": new_df.loc[x, "profit"]})
             list_action.append(action)
 
         return list_action
 
-    def choice_action(self, list_sorted: list) -> list:
-        """choice the action budget = 500"""
-        total_benefit_max = 0
-        best_list = None
-        for index in range(len(list_sorted)):
-            list_choice = []
-            budget = 500
-            total_benefit = 0
-            for nombre in list_sorted:
-                unit_yield_max = 0
-                action_save = None
-                for action in list_sorted:
-                    if action not in list_choice and action.unit_yield >= unit_yield_max and\
-                            action.cost in [action.cost for action in list_sorted][0:index]\
-                            and budget // action.cost >= 1:
-                        unit_yield_max = action.unit_yield
-                        action_save = action
-                if action_save:
-                    budget = budget - action_save.cost
-                    total_benefit += action_save.benefit
-                    list_choice.append(action_save)
-            if total_benefit >= total_benefit_max:
-                best_list = list_choice.copy()
-                total_benefit_max = total_benefit
-        return best_list
+    def knap_sack(self, list_sorted: list):
+        budget = 500*100
+        n = len(list_sorted)
+        list_cost = [action.cost for action in list_sorted]
+        list_benefit = [action.benefit for action in list_sorted]
+        K = [[0 for x in range(budget + 1)] for x in range(n + 1)]
+        for i in range(n + 1):
+            for w in range(budget + 1):
+                if i == 0 or w == 0:
+                    K[i][w] = 0
+                elif list_cost[i - 1] <= w:
+                    K[i][w] = max(list_benefit[i - 1] + K[i - 1][w - list_cost[i - 1]], K[i - 1][w])
+                else:
+                    K[i][w] = K[i - 1][w]
+
+        total_benefice = K[n][budget]
+        budget = 500 * 100
+        n = len(list_sorted)
+        list_action = []
+        while budget >= 0 and n >= 0:
+            action = list_sorted[n-1]
+            if K[n][budget] == K[n - 1][budget-action.cost] + action.benefit:
+                list_action.append(action)
+                budget -= action.cost
+            n -= 1
+
+        return list_action, total_benefice
 
     def run(self, file):
         """launch the program"""
         start = time.time()
         list_action = self.import_csv(file)
-        list_choice = self.choice_action(list_action)
-        self.view.display_list(list_choice)
+        list_choice, total = self.knap_sack(list_action)
+        self.view.display_list(list_choice, total)
         end = time.time()
         duration = end - start
         self.view.display_time(duration)
@@ -81,17 +83,17 @@ class Controller:
 
 class View:
 
-    def display_list(self, data: list):
+    def display_list(self, data: list, total: float):
         """display a list"""
         line = 0
         total_cost = 0
         total_benefit = 0
         for row in data:
-            print(f"{row.name :10} {row.cost :4} {row.rate :4} {row.benefit :6}")
+            print(f"{row.name :10} {row.cost/100 :4} {row.rate :4} {row.benefit/100 :6}")
             line += 1
-            total_cost += row.cost
-            total_benefit += row.benefit
-        print(f"cout total: {total_cost :5} total benefice: {total_benefit :5} nb_action: {line :2}")
+            total_cost += row.cost/100
+
+        print(f"cout total: {total_cost :5} total benefice: {total/100 :5} nb_action: {line :2}")
 
     def display_time(self, duration: float):
         print(f"duree du script : {duration :10}")
